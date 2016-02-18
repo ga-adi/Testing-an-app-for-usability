1c32ffcc7065b4081a07c9622edd196e768f7235
acjones212 <acjones212@gmail.com>
2016-02-18 17:28:07 -0500

    Usability

--- starter-code/UsabilityTestingStarterCode/app/src/main/java/generalassembly/yuliyakaleda/usabilitytestingstartercode/DetailsFragment.java
+++ starter-code/UsabilityTestingStarterCode/app/src/main/java/generalassembly/yuliyakaleda/usabilitytestingstartercode/DetailsFragment.java
@@ -1,26 +1,34 @@
 package generalassembly.yuliyakaleda.usabilitytestingstartercode;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 
-public class DetailsFragment extends Fragment{
+public class DetailsFragment extends Fragment {
 
-  @Nullable @Override
-  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-      @Nullable Bundle savedInstanceState) {
-    //TODO: Inflate the view and change the return type;
-    return null;
-  }
+    private WebView mWebView;
 
-  public void updateContent(String sign) {
-    // TODO: Finish the method which will open a webview and redirect the user to the website
-    // TODO: to read about the sign that was clicked in the ListView
-  }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        MainActivity mainActivity = (MainActivity) getActivity();
+        String url = "http://www.horoscopedates.com/zodiac-signs/" + mainActivity.getUrl() + "/";
+        View view = inflater.inflate(R.layout.details_activity_layout, container, false);
+        Log.i("URL", url);
+        updateContent(view, url);
+        return view;
+    }
+
+    public void updateContent(View view, String sign) {
+        // TODO: Finish the method which will open a webview and redirect the user to the website
+        // TODO: to read about the sign that was clicked in the ListView
+        ((WebView) view.findViewById(R.id.webview)).loadUrl(sign);
+    }
 }
--- starter-code/UsabilityTestingStarterCode/app/src/main/java/generalassembly/yuliyakaleda/usabilitytestingstartercode/MainActivity.java
+++ starter-code/UsabilityTestingStarterCode/app/src/main/java/generalassembly/yuliyakaleda/usabilitytestingstartercode/MainActivity.java
@@ -1,16 +1,39 @@
 package generalassembly.yuliyakaleda.usabilitytestingstartercode;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+  private String mURL = "http://www.google.com";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+    ListFragment listFragment = new ListFragment();
+    fragmentTransaction.add(R.id.frame_layout, listFragment);
+    fragmentTransaction.commit();
+
     //TODO: Create a separate layout xml file for tablet devices.
+  }
+
+  public void swapFragment(String listItem){
+    this.mURL = listItem;
+    DetailsFragment detailsFragment = new DetailsFragment();
+    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+    transaction.replace(R.id.frame_layout, detailsFragment);
+    transaction.addToBackStack(null);
+    transaction.commit();
+  }
+
+  public String getUrl(){
+    return mURL;
   }
 }
