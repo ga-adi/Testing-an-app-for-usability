7b6baab88c34a4df98ca452791093e7a799f5035
chris-shum <shum.chris@yahoo.com>
2016-02-18 17:02:23 -0500

    Improved App

--- starter-code/UsabilityTestingStarterCode/app/src/main/java/generalassembly/yuliyakaleda/usabilitytestingstartercode/ListFragment.java
+++ starter-code/UsabilityTestingStarterCode/app/src/main/java/generalassembly/yuliyakaleda/usabilitytestingstartercode/ListFragment.java
@@ -13,38 +13,44 @@ import android.widget.ListView;
 
 
 public class ListFragment extends Fragment {
-  private static final String SIGN = "sign";
-  private ListView listView;
+    private static final String SIGN = "sign";
+    private ListView listView;
 
-  @Nullable @Override
-  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-      @Nullable Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_layout, container, false);
-    listView = (ListView) view.findViewById(R.id.list_view);
-    return view;
-  }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-  @Override
-  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
-    String[] values = getResources().getStringArray(R.array.signs);
-    ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
-        android.R.layout.simple_list_item_1, android.R.id.text1, values);
-    listView.setAdapter(adapter);
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_layout, container, false);
+        listView = (ListView) view.findViewById(R.id.list_view);
+        return view;
+    }
 
-    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-      // TODO: Write the logic that will check if the DetailsFragment is present next to the
-      // TODO: ListFragment or not. If it is not (it is a handset), start DetailsActivity. If
-      // TODO: it is present, get reference to DetailsFragment and call a method on it that will
-      // TODO: open a webview with the information about the clicked sign.
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        String[] values = getResources().getStringArray(R.array.signs);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listView.setAdapter(adapter);
 
-      @Override
-      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String itemValue = (String) listView.getItemAtPosition(position);
-        Intent intent = new Intent(getActivity(), DetailsActivity.class);
-        intent.putExtra(SIGN, itemValue);
-        startActivity(intent);
-      }
-    });
-  }
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            // TODO: Write the logic that will check if the DetailsFragment is present next to the
+            // TODO: ListFragment or not. If it is not (it is a handset), start DetailsActivity. If
+            // TODO: it is present, get reference to DetailsFragment and call a method on it that will
+            // TODO: open a webview with the information about the clicked sign.
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemValue = (String) listView.getItemAtPosition(position);
+                Intent intent = new Intent(getActivity(), DetailsActivity.class);
+                intent.putExtra(SIGN, itemValue);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- starter-code/UsabilityTestingStarterCode/app/src/main/java/generalassembly/yuliyakaleda/usabilitytestingstartercode/MainActivity.java
+++ starter-code/UsabilityTestingStarterCode/app/src/main/java/generalassembly/yuliyakaleda/usabilitytestingstartercode/MainActivity.java
@@ -1,16 +1,26 @@
 package generalassembly.yuliyakaleda.usabilitytestingstartercode;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    //TODO: Create a separate layout xml file for tablet devices.
-  }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        ListFragment listFragment = new ListFragment();
+        fragmentTransaction.add(R.id.fragment_container, listFragment);
+        fragmentTransaction.commit();
+
+
+
+        //TODO: Create a separate layout xml file for tablet devices.
+    }
 }
